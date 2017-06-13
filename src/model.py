2e85dd6c3c41063fab5bfc097b4d7ad9519ca421
Guilherme Pombo <guilhermepombo@Guilhermes-MacBook-Pro.local>
2017-06-13 18:39:24 +0100

    notebook for hyperparameter optimisation

--- src/model.py
+++ src/model.py
@@ -40,6 +40,45 @@ def train_boosted_tree(df_train, features_to_use, labels, val_size=None, num_epo
     return bst
 
 
+def param_hyperopt(params_list, df_train, features_to_use, labels, val_size, num_epochs=200, early_stopping=10):
+    """
+    Try various parameter configurations and choose the one that obtains the best results on the validation data
+    :param params_list: list of parameters to try
+    :param df_train:
+    :param features_to_use:
+    :param labels:
+    :param val_size:
+    :param num_epochs:
+    :param early_stopping:
+    :return: the dictionary with the representation of the best parameters
+    """
+    print('formating for lgb')
+    print("Using a validation set for early stopping")
+
+    df_train, labels, df_val, val_labels = split_train_val(df=df_train, labels=labels, val_size=val_size)
+    d_val = lgb.Dataset(df_val[features_to_use],
+                        label=val_labels,
+                        categorical_feature=['aisle_id', 'department_id'])  # , 'order_hour_of_day', 'dow'
+
+    d_train = lgb.Dataset(df_train[features_to_use],
+                          label=labels,
+                          categorical_feature=['aisle_id', 'department_id'])  # , 'order_hour_of_day', 'dow'
+
+    gc.collect()
+
+    best_score = 0
+    best_params = None
+    for params in params_list:
+        print('Training boosted tree with parameters: {}'.format(params))
+        bst = lgb.train(params, d_train, num_epochs, valid_sets=[d_val], early_stopping_rounds=early_stopping)
+        tmp_best_score = bst.best_score['valid_0']['auc']
+        if tmp_best_score > best_score:
+            best_score = tmp_best_score
+            best_params = params
+
+    return best_params, best_score
+
+
 def filter_by_confidence(df_test, test_orders, threshold=0.25):
     """
     Filter submissions by a confidence threshold
